724b64bf86917b87c158c501cb3926a952d59f2f
Kshitij-Dhakal <dhakalkshitij@gmail.com>
2019-08-17 15:20:40 +0545

    Code Separation

--- src/dependencies/lib/Config.java
+++ src/dependencies/lib/Config.java
@@ -1,7 +1,7 @@
 package dependencies.lib;
 
 public class Config {
-    public static String VERSION = "1.0.0";
+    public static String VERSION = "1.0.1";
     public static int PORT = 42069;
     public static int VIDEO_CALLER_PORT = 42070;
     public static int VIDEO_RECEIVER_PORT = 42071;
